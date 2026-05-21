42e22f42bed6ffd8943aa20cf6b63270d194dddc
Victormoroo <victorhugo0609@outlook.com>
2026-05-21 12:03:45 -0300

    chore: estrutura inicial do projeto Maven com Spring Boot 4

---
+++ src/main/java/br/dev/javacom/JavaComApiApplication.java
@@ -0,0 +1,14 @@
+package br.dev.javacom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class JavaComApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaComApiApplication.class, args);
+    }
+}
